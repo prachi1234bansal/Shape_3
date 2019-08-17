25de123c46d51867a512a29ac6eee905c7f3f9c4
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2019-08-17 20:11:56 +0530

    Add files via upload

---
+++ shape_3.java
@@ -0,0 +1,25 @@
+class Shape_3
+{
+	public static void main(String args[])
+	{
+		int i,j,k;
+		int s=3;
+		for(i=1;i<=4;i++)
+		{
+			for(k=0;k<s;k++)
+			{
+				System.out.print(" ");
+			}
+					for(j=1;j<=i*2-1;j++)
+					{
+						System.out.print("*");
+					}
+					System.out.println();
+					s--;
+				}
+				
+				}
+	}
+
+
+
